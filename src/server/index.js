4543637e5c30409a2cb4e6d6118512a8d140c16b
Altamimi <77057341+Al-Salama@users.noreply.github.com>
2024-01-05 00:30:44 +0300

    Minor changes, install dependencies.
    -Update server port.
    -Improve how static files are served [Get rid of app.get("/", ...)].

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,27 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
 
-const app = express()
+const app = express();
 
-app.use(express.static('src/client'))
+app.use(
+  express.static("src/client", {
+    index: "/views/index.html",
+  })
+);
 
-console.log(__dirname)
+console.log(__dirname);
 
-app.get('/', function (req, res) {
-    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
-})
+/* app.get("/", function (req, res) {
+  res.sendFile("/client/views/index.html", { root: __dirname + "/.." });
+}); */
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+const PORT = 1506;
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
